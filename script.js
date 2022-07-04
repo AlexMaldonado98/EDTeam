c95908e2a96b7588e9a388b51495c1b5a169cc04
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-07-04 14:01:10 -0500

    total-sections-3/navbar-mobile-finished/

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const navBar =  document.querySelector('.container');
 
 const showNavbar = () => {
-    navBar.classList.toggle('d-none');
+    setTimeout(() =>{
+        navBar.classList.toggle('d-none');
+    },300);
+
+    setTimeout(() =>{
+        navBar.classList.toggle('animationOn');
+    },310);
+
+    navBar.classList.toggle('animationOff');
 
 }
